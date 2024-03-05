e63bb9f775d3fae181ac6d8acce9c27faf7d5f15
AoiYamashita <154947689+AoiYamashita@users.noreply.github.com>
2024-03-05 19:31:53 +0900

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,15 +1,20 @@
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install curl gnupg2 lsb-release nano python3-dev python3-setuptools python3-pip -y
+sudo apt update && sudo apt install curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-sudo apt update -y
-sudo apt upgrade -y 
-sudo apt install ros-foxy-desktop -y
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt install ros-foxy-desktop
 
 echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+
+sudo apt install python3-colcon-common-extensions
+sudo apt install python3-argcomplete
+
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws
+colcon build
 
 sudo curl -sSL http://get.gazebosim.org | sh
 sudo apt install ros-foxy-gazebo-* -y
